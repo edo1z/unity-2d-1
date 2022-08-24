f63b5f5398b1e0a2ae118c41e0de1116bbf33ff7
web3ten0 <web3ten0@gmail.com>
2022-08-24 23:57:50 +0900

    added Ball animation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Rigidbody2D _rigidbody2D = null;
+    public Rigidbody2D RigidBody {
+        get { return _rigidbody2D ?? (_rigidbody2D = gameObject.GetComponent<Rigidbody2D> ()); }
     }
 
-    // Update is called once per frame
+    SpriteRenderer _renderer = null;
+    public SpriteRenderer Renderer {
+        get { return _renderer ?? (_renderer = gameObject.GetComponent<SpriteRenderer> ()); }
+    }
+
+    float sprite_w;
+    float sprite_h;
+    Vector2 screen_max;
+    Vector2 screen_min;
+
+    void Start()
+    {
+        sprite_w = Renderer.bounds.size.x;
+        sprite_h = Renderer.bounds.size.y;
+        screen_max = Camera.main.ViewportToWorldPoint(Vector2.one);
+        screen_min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+
+        float direction = Random.Range(0, 359);
+        float speed = 10;
+        Vector2 v;
+        v.x = Mathf.Cos(Mathf.Deg2Rad * direction) * speed;
+        v.y = Mathf.Sin(Mathf.Deg2Rad * direction) * speed;
+        RigidBody.velocity = v;
+        Debug.Log(v.x + " " + v.y);
+    }
+
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        Vector2 v = RigidBody.velocity;
+        if (position.x < screen_min.x + sprite_w / 2 && v.x < 0) {
+            v.x *= -1;
+        } else if (position.x > screen_max.x - sprite_w / 2 && v.x > 0) {
+            v.x *= -1;
+        }
+        if (position.y < screen_min.y + sprite_h / 2 && v.y < 0) {
+            v.y *= -1;
+        } else if (position.y > screen_max.y - sprite_h / 2 && v.y > 0) {
+            v.y *= -1;
+        }
+        RigidBody.velocity = v;
     }
 }
